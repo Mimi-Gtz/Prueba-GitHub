1c8826afb6e61939e20f90b3de443be050aa896d
Creativos <VeraGtz@macbookveragtz.local>
2016-02-26 23:21:10 -0500

    Initial Commit

---
+++ Primera Aplicacion Mimi 1Tests/Primera_Aplicacion_Mimi_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Primera_Aplicacion_Mimi_1Tests.swift
+//  Primera Aplicacion Mimi 1Tests
+//
+//  Created by Creativos on 26/02/16.
+//  Copyright © 2016 Mimi-Gtz. All rights reserved.
+//
+
+import XCTest
+@testable import Primera_Aplicacion_Mimi_1
+
+class Primera_Aplicacion_Mimi_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
